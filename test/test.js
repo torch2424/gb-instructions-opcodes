287c1aa6567776fc50e625decfe02dc99d05c63a
Aaron Turner <aaron@aaronthedev.com>
2019-01-17 01:56:02 -0800

    Fixed some missing opcodes

--- test/test.js
+++ test/test.js
@@ -1,10 +1,31 @@
 
 const {GBOpcodes} = require('../dist/gb-instructions-opcodes.cjs');
 
+const missingHexIndexes = [0xD3, 0xE3, 0xE4, 0xF4, 0xDB, 0xEB, 0xEC, 0xFC, 0xDD, 0xED, 0xFD, 0xCB];
+
 var assert = require('assert');
 describe('GB Instructions Opcodes', function() {
+
   it('should get generated', () => {
     assert.equal(!!GBOpcodes.opcodes, true);
     assert.equal(!!GBOpcodes.cbOpcodes, true);
+  });
+
+  it('should only be missing some specified hex indexes', () => {
+
+    const checkAllValues = (opcodeCollection, missingIndexes, messageHeader) => {
+      for (let i = 0; i <= 0xFF; i++) {
+        if (!missingIndexes.includes(i)) {
+          assert.equal(
+            !!opcodeCollection[GBOpcodes.getNumberAsOpcodeHex(i)], 
+            true, 
+            `${messageHeader} ${GBOpcodes.getNumberAsOpcodeHex(i)} does not exist`
+          );
+        }
+      }
+    }
+
+    checkAllValues(GBOpcodes.opcodes, missingHexIndexes, 'Opcodes:');
+    checkAllValues(GBOpcodes.cbOpcodes, [], 'CB Opcodes:');
   });
 });
